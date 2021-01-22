f7432f2adf9778375e521de01f087e83c98edb0b
inouekatou <ino0607soccer@gmail.com>
2021-01-22 19:25:43 +0900

    add to global state

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Layout from '../components/Layout/Layout'
 import { fetchPopularData } from '../apis/index'
+import { Store } from '../store/index'
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store)
   useEffect(() => {
     fetchPopularData().then((res) => {
       console.log('data', res)
+      setGlobalState({type: 'SET_POPULAR', payload: {popular: res.data.items}})
     })
   }, [])
   return (
